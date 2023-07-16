507d0dabd79f69f11426aa6b70cfd0330583a5e7
jaishankar <jaishankarg24@gmail.com>
2023-07-16 18:39:02 +0530

    Ducks pattern

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 //console.log("Hello World!");
 // Redux Fundamentals
-import store from "./store";
-import { bugAdded, bugRemoved, bugResolved } from "./actions";
+import configureStore from "./store/configureStore";
+//import { bugAdded, bugRemoved, bugResolved } from "./store/actions";
+import * as actions from "./store/bugs";
+
+const store = configureStore();
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
-store.dispatch(bugAdded("Bug1"));
-store.dispatch(bugAdded("Bug2"));
+store.dispatch(actions.bugAdded("Bug1"));
+store.dispatch(actions.bugAdded("Bug2"));
 
-store.dispatch(bugResolved(1));
+store.dispatch(actions.bugResolved(1));
 
-store.dispatch(bugRemoved(1));
+store.dispatch(actions.bugRemoved(1));
 
 console.log(store.getState());
 
-store.dispatch(bugAdded("Bug1"));
+store.dispatch(actions.bugAdded("Bug1"));
 
 // store.dispatch({ type: "bugAdded", payload: { description: "Bug1" } });
 // unsubscribe();
---
+++ src/store/configureStore.js
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+import reducer from "./bugs";
+import { devToolsEnhancer } from "redux-devtools-extension";
+
+export default function configureStore() {
+  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
+  return store;
+}
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : allow us to talk to Devtools
+// const store =createStore(
+//   reducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+// export default store;
+
+// const store = createStore(reducer, devtoolsEnhancer({ trace: true })); : allow us to talk to Devtools with Tracing Enabled
